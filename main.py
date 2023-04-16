44b1f3b5ece01f05e6cad77262cae5605fbc87d4
Doğukan <dogukanakgun123@hotmail.com>
2023-04-16 20:13:54 +0300

    new services for advertise

--- main.py
+++ main.py
@@ -37,10 +37,12 @@ def get_sequence(seq_name):
     return mydb.counters.find_and_modify(query={"_id": seq_name}, update={"$inc": {"seq": 1}}, upsert=True)["seq"]
 """
 
+
 @app.route('/script')
 def script():
     # TODO: burası sonradan veri doldurmak için script yazılacak
     return 0
+
 
 @app.route('/signin', methods=['POST'])
 def signin():
@@ -55,35 +57,41 @@ def signin():
     usersTable.insert_one(user)
     return {"resultCode":"Ok"}
 
+
 # ana sayfa doldurma
 @app.route('/vehicles')
 def vehicles():
     vehicleData = {"res": list(vehiclesTable.find({}))}
     return vehicleData
 
+
 @app.route('/brands')
 def brands():
     brandData = {"res": list(otomobilBrandsTable.find({}))}
     return brandData
+
 
 @app.route('/motorcyclebrands')
 def motorcycleBrands():
     motorcyleBrandData = {"res": list(motorcycleBrandsTable.find({}))}
     return motorcyleBrandData
 
+
 @app.route('/suvbrands')
 def suvBrands():
     suvBrandData = {"res": list(suvBrandsTable.find({}))}
     return suvBrandData
 
+
 @app.route('/cardetails', methods=['POST'])
 def carDetails():
     data = request.get_json()
-    print("data ne",data)
+
     id = data['_id']
     carDetailsData = {"res": list(carDetailTable.find({'_id': id}))}
-    print("car detail ne : ",carDetailsData)
+
     return jsonify(carDetailsData)
+
 
 @app.route('/profile', methods=['POST'])
 def userDetail():
@@ -95,9 +103,19 @@ def userDetail():
 @app.route('/vehiclelist', methods=['POST'])
 def vehicleList():
     req = request.get_json()
+
     vehicleListData = {"res": list(vehicleListTable.find({"brand": req["brand"], "type": req["type"]}))}
-    print("data", vehicleListData)
+
     return vehicleListData
+
+@app.route('/ownvehiclelist', methods=['POST'])
+def ownvehicleList():
+    req = request.get_json()
+    print("req ney",req)
+    ownvehicleListData = {"res": list(vehicleListTable.find({"created_by": req["created_by"]}))}
+
+    return ownvehicleListData
+
 
 # TODO: Burda kişin favorilerinin içindekileri alıp gelmesi gerekiyor
 
@@ -113,6 +131,7 @@ def favoritesList():
     print("list", favData)
     return jsonify(favData)
 """
+
 
 @app.route('/favoritesList',methods=['POST'])
 def favoritesList():
@@ -132,11 +151,44 @@ def favoritesList():
 @app.route('/vehicleadd', methods=['POST'])
 def vehicleAdd():
     data = request.get_json()
-    id = data['_id']
+
+    if data["category"] == "Otomobil":
+        data["category"] = "car"
+    elif data["category"] == "SUV":
+        data["category"] = "suv"
+    elif data["category"] == "Motorsiklet":
+        data["category"] = "motorcycle"
+
+
+    countofList = vehicleListTable.count_documents({})
+
+    countofDetail = carDetailTable.count_documents({})
+
+    data["_id"] = countofList + 1
+
+    data["ad_id"] = countofDetail + 1
+
+    #vehicleListTable.insert_one(data)
+    print("listeye eklenecek olan",data)
+
+    data["_id"] = countofDetail + 1
+
+    del data["ad_id"]
+    #carDetailTable.insert_one(data)
+    print("detail eklenecek olan ",data)
+
+    """
+     id = data['_id']
     title = data['title']
     vehiclesTable.insert_one({'_id': id, 'title': title})
+    """
+
     return {"resultCode": "Ok"}
 
+@app.route('/file', methods=['POST'])
+def file():
+    print("sern nesin be abi:")
+    return {"resultCode": "Ok"}
 
 @app.route('/vehicledelete', methods=['POST'])
 def vehicleDelete():
